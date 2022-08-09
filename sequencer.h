b7802485eb353e2882be64ca2af13a547c6c651f
ZDH <denizh@desird.com>
2022-08-09 10:27:21 +0300

    Sequencer 3 added. set and get time functions united.

--- sequencer.h
+++ sequencer.h
@@ -18,19 +18,26 @@ typedef struct
     uint8_t current_step;
 } sequence;
 
+void Sequencer_reset(sequence *seq);
+void Sequencer_set_step1_time(sequence *seq, unsigned long time);
+void Sequencer_set_step2_time(sequence *seq, unsigned long time);
+void Sequencer_set_step3_time(sequence *seq, unsigned long time);
+// void Sequencer_set_step4_time(sequence *seq, unsigned long time);
+unsigned long Sequencer_get_step1_time(sequence *seq);
+unsigned long Sequencer_get_step2_time(sequence *seq);
+unsigned long Sequencer_get_step3_time(sequence *seq);
+// unsigned long Sequencer_get_step4_time(sequence *seq);
+
 void Sequencer1(sequence *seq, void (*step1)(), unsigned long time1);
-void Sequencer1_reset(sequence *seq);
 void Sequencer1_run(sequence *seq);
-void Sequencer1_set_step1_time(sequence *seq, unsigned long time);
-unsigned long Sequencer1_get_step1_time(sequence *seq);
 
 void Sequencer2(sequence *seq, void (*step1)(), unsigned long time1,
                 void (*step2)(), unsigned long time2);
-void Sequencer2_set_step1_time(sequence *seq,unsigned long time);
-void Sequencer2_set_step2_time(sequence *seq, unsigned long time);
-unsigned long Sequencer2_get_step1_time(sequence *seq);
-unsigned long Sequencer2_get_step2_time(sequence *seq);
-void Sequencer2_reset(sequence *seq);
 void Sequencer2_run(sequence *seq);
+
+void Sequencer3(sequence *seq, void (*step1)(), unsigned long time1,
+                void (*step2)(), unsigned long time2,
+                void (*step3)(), unsigned long time3);
+void Sequencer3_run(sequence *seq);
 
 #endif
